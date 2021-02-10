09c8f1d6688292c1f38d9241fd3ae4c5b6d5775f
Armando Brito <arebrif@gmail.com>
2021-02-09 21:33:34 -0400

    feat: slider in example project
    add a slider control to manipulate the progress view

--- Example/ABCircularProgressView/ViewController.swift
+++ Example/ABCircularProgressView/ViewController.swift
@@ -12,17 +12,27 @@ import ABCircularProgressView
 class ViewController: UIViewController {
     
     var progressView: ABCircularProgressView!
+    
+    var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        progressView = ABCircularProgressView(frame: CGRect(x: 0, y: 0, width: 60, height: 60), barColor: .systemBlue)
+        progressView = ABCircularProgressView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 60, height: 60)), barColor: .systemBlue)
         progressView.setProgress(percentage: 75)
-        
-        progressView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(progressView)
-        
         progressView.center = view.center
+        
+        slider = UISlider(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: view.bounds.width / 2, height: 50)))
+        slider.setValue(0.75, animated: false)
+        slider.addTarget(self, action: #selector(sliderChanged(sender:forEvent:)), for: .valueChanged)
+        slider.isContinuous = true
+        view.addSubview(slider)
+        slider.frame = slider.frame.offsetBy(dx: slider.frame.width / 2, dy: progressView.frame.maxY + 25)
+    }
+    
+    @objc func sliderChanged(sender: UISlider, forEvent event: UIEvent) {
+        progressView.setProgress(sender.value)
     }
 
 }
